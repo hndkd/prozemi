0ea6745a6a1a64eb6d6801eac25734e5fe3089c6
hndkd <takoshomi0817@outlook.jp>
2022-02-09 08:27:26 +0000

    basic signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
 end
